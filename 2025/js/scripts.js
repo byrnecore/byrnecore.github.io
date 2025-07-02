61a9b4cc6fecaa590746dc53cd74275f03f15fb0
Shaun Byrne <5062580+byrnecore@users.noreply.github.com>
2025-07-02 11:34:10 +1000

    backup of the last version of the portfolio
    backup of the last version of the portfolio

---
+++ 2025/js/scripts.js
@@ -0,0 +1,24 @@
+
+// Smooth Scroll to anchors
+
+var scroll = new SmoothScroll('a[href*="#"]');
+
+
+// ScrollReveal
+
+ScrollReveal().reveal('.reveal', { duration: 2000 }, { delay: 1500 });
+ScrollReveal().reveal('.heroreveal', { duration: 3000 });
+
+
+// Burger Toggle
+
+var $hamburger = $(".hamburger");
+	$hamburger.on("click", function(e) {
+	$hamburger.toggleClass("is-active");
+	// Do something else, like open/close menu
+});
+
+$('#burger_btn').click(function(e){
+	e.preventDefault();
+	$('#mainNav').toggleClass('menuOpen');
+});
